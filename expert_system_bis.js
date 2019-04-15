50a5d86cb5b3e99d79bee5617d4fe05ebd695df1
david <david.tabarie@gmail.com>
2019-04-15 12:15:45 +0200

    add second knowledge base

---
+++ expert_system_bis.js
@@ -0,0 +1,70 @@
+// Try edit msg
+var nbCote = 3;
+var nbAnglesDroits = 0;
+var nbCotesPara = 0 ;
+var nbCoteMemeLg = 2;
+var trouve = 0 ;
+
+if (nbCote > 1){
+    if (nbCote == 3){
+	// triangle
+	document.write('Triangle');
+	exit();
+	if (nbCoteMemeLg >= 2){
+	    // isocèle
+	    document.write('Triangle isocèle');
+	    if (nbAnglesDroits == 1) {
+		// tri rect isocèle
+		document.write('Triangle rectangle isocèle');
+	    }
+	    if (nbCoteMemeLg == 3){
+		// tri equilateral
+		document.write('Triangle equilatéral');
+	    }
+	}
+	if (nbAnglesDroits == 1){
+	    // tri rect
+	    document.write('Triangle rectangle');
+	}
+    }
+    if (nbCote == 4){
+	// Quadrilateral
+	document.write('Quadrilatère');
+	if (nbCotesPara >= 2){	    
+	    // trapeze (A trapezoid is a quadrilateral with exactly one pair of parallel sides)
+	    document.write('Trapèze');
+	    if (nbCotesPara == 4){
+		// parallelogramme
+		document.write('Parallelogramme');
+		if (nbAnglesDroits == 4){
+		    // rectangle
+		    document.write('Rectangle');
+		    if (nbCoteMemeLg == 4){
+			// carré
+			document.write('Carré');
+		    }
+		}
+		if (nbCoteMemeLg == 4){		    
+		    // losange
+		    document.write('Losange');
+		}	    
+	    }
+	}	
+	if (nbCoteMemeLg == 2){
+	    if (nbAnglesDroits == 0){
+		// Kite (kite A kite is a quadrilateral with two distinct pairs of adjacent sides that are congruent.)
+		document.write('Kite');
+	    }
+	}
+    }       
+}
+
+if(nbCote==5){
+
+    exit();
+}
+if(nbCote==6){}
+if(nbCote==8){}
+if(nbCote==10){}
+if(nbCote==12){}
+if(nbCote==20){}
