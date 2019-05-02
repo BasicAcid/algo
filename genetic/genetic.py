764cc63c91dad9e2065f0de85ca4b57d20883d9b
BasicAcid <david.tabarie@gmail.com>
2019-05-02 22:28:38 +0200

    better

--- genetic/genetic.py
+++ genetic/genetic.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
-import random, json, struct, geopy.distance
-from math import sin, cos, sqrt, atan2, radians
+import random, json, struct, geopy.distance, math
+
+# Currently not used
+INDIVIDUALS_BY_GENERATION = 200
 
 # Crossover rate in percents
 CROSSOVER_RATE = 80
@@ -11,15 +13,6 @@ MUTATION_RATE = 0,5
 
 data = json.load(open('cities.json', 'r'))
 
-for each in data:
-    print(each)
-
-class City(object):
-    def __init__(self):
-        self.name = name
-        self.lan = lan
-        self.lng = lng
-
 def float_to_bin(num):
     return format(struct.unpack('!I', struct.pack('!f', num))[0], '032b')
 
@@ -27,18 +20,54 @@ def bin_to_float(binary):
     return struct.unpack('!f',struct.pack('!I', int(binary, 2)))[0]
 
 def rand_population():
-    """Generate a list of solutions"""
+    """
+    Generate a list of solutions
+    Return format: rand_population[row][column]
+    """
     initial_population = []
     for line in range(200):
-        rand = random.sample(range(10), 10)
+        rand = random.sample(range(15), 15)
         initial_population.append(rand)
     return initial_population
 
+rand_population = rand_population()
+
 def cities_distance(city1, city2):
-    coords_1 = (52.2296756, 21.0122287)
-    coords_2 = (52.406374, 16.9251681)
+    """
+    Get distance between two cities, take the row number as arguments
+    Usage: cities_distance(0,4)
+    """
+    coords_1 = ( float( data[city1]['lan'] ), float( data[city1]['lng'] ) )
+    coords_2 = ( float( data[city2]['lan'] ), float( data[city2]['lng'] ) )
     return geopy.distance.geodesic(coords_1, coords_2).km
 
-def individual_fitness(solution):
+def get_chromosome(row):
+    """Return only one individual from rand_population"""
+    individual = []
+    for cities in range( len( rand_population[row] ) ):
+        individual.append(rand_population[row][cities])
+    return individual
+
+def individual_fitness(individual):
     """Evaluate the fitness of one individual"""
+    pass_value = []
+    # Add starting point at the end of the pass
+    individual.append(individual[0])
+    for i in range( len( individual ) -1):
+        pass_value.append( cities_distance( individual[i], individual[i+1] ) )
+    # Add fitness to the end of the list
+    individual.append(sum(pass_value))
+    return individual
+
+def population_fitness(population):
+    """Evaluate the fitness of a complete population"""
+    population_with_fitness = []
+    for members in range(len(population)):
+        population_with_fitness.append(individual_fitness(get_chromosome(members)))
+    return population_with_fitness
+
+population_fitness(rand_population)
+
+def classify_population():
+    """Classify a population by scores (best on top)"""
     pass
